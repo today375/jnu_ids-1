2e9f27762c142935ec4c3a881a986feb6e3cde6d
채두걸 <sianux1209@naver.com>
2016-05-23 04:14:05 +0900

    ImageResourse 추가,
    소공론 넘나 힘든것

--- src/Resource/ImageResource.java
+++ src/Resource/ImageResource.java
@@ -6,21 +6,20 @@
 //
 //
 
+package Resource;
 
-
-package IDS.Resource;
-
-
+//이미지명은 View별로 함수 안에 지역변수로 여기에 이름 넣어서 사용
 public class ImageResource {
-	public String getMainViewResource(Object String) {
 	
-	}
+	//MainView imageResource start
+	public final static String mainView_title = "res\\MainView\\icon_title.png";
+	public final static String mainView_start = "res\\MainView\\icon_start.png";
+	public final static String mainView_stop = "res\\MainView\\icon_stop.png";
+	public final static String mainView_info_title = "res\\MainView\\icon_info.png";
+	public final static String mainView_analysis = "res\\MainView\\icon_analysis.png";
+	public final static String mainView_traffic = "res\\MainView\\icon_traffic.png";
+	public final static String mainView_eventLog = "res\\MainView\\icon_eventlog.png";
+	public final static String mainView_Background = "res\\MainView\\icon_background.png";
+	//MainViewImageResource end
 	
-	public String getAnalysesViewResource(Object String) {
-	
-	}
-	
-	public String getPreferencesViewResource(Object String) {
-	
-	}
 }
